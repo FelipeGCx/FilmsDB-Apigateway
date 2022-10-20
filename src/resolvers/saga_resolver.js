2c89d9debe71cf702ee8f23be3aa0541ab100db3
FelipeGCx <ggfelipegc@gmail.com>
2022-10-19 23:18:18 -0500

    fixed the authorization header

--- src/resolvers/saga_resolver.js
+++ src/resolvers/saga_resolver.js
@@ -1,30 +1,30 @@
 const sagaResolver = {
-	Query: {
-		getSaga: (_, { sagaId }, { dataSources }) => {
-			return dataSources.sagaAPI.getSaga(sagaId);
-		},
-		getSagas: (_, __, { dataSources }) => {
-			return dataSources.sagaAPI.getSagas();
-		}
-	},
-	Mutation: {
-		createSaga: (_, { sagaInput }, { dataSources }) => {
-			const saga = {
-				saga: sagaInput.saga,
-				svg: sagaInput.svg
-			};
-			return dataSources.sagaAPI.createSaga(saga);
-		},
-        updateSaga: (_, { sagaId,sagaInput }, { dataSources }) => {
-            const saga = {
-				saga: sagaInput.saga || null,
-				svg: sagaInput.svg || null
-			};
-			return dataSources.sagaAPI.updateSaga(sagaId,saga);
-		},
-		deleteSaga: (_, { sagaId }, { dataSources }) => {
-			return dataSources.sagaAPI.deleteSaga(sagaId);
-		},
-	}
+  Query: {
+    getSaga: (_, { sagaId }, { dataSources }) => {
+      return dataSources.sagaAPI.getSaga(sagaId);
+    },
+    getSagas: (_, __, { dataSources }) => {
+      return dataSources.sagaAPI.getSagas();
+    },
+  },
+  Mutation: {
+    createSaga: (_, { sagaInput }, { dataSources, user }) => {
+      const saga = {
+        saga: sagaInput.saga,
+        svg: sagaInput.svg,
+      };
+      return dataSources.sagaAPI.createSaga(saga, user.accessToken);
+    },
+    updateSaga: (_, { sagaId, sagaInput }, { dataSources, user }) => {
+      const saga = {
+        saga: sagaInput.saga || null,
+        svg: sagaInput.svg || null,
+      };
+      return dataSources.sagaAPI.updateSaga(sagaId, saga, user.accessToken);
+    },
+    deleteSaga: (_, { sagaId }, { dataSources, user }) => {
+      return dataSources.sagaAPI.deleteSaga(sagaId, user.accessToken);
+    },
+  },
 };
 module.exports = sagaResolver;
