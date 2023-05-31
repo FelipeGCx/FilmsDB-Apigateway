4c3eaf4c51ac520b59042e0b3eaeaf11d78254ac
felipe gomez <ggfelipegc@gmail.com>
2023-05-31 15:46:40 -0500

    changed url backend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 module.exports = {
 	// films_api_url: 'http://localhost:8080/api/v1',
-	films_api_url: 'https://filmsdb-ms-production.up.railway.app/api/v1',
+	films_api_url: 'https://filsmdb-microservice.onrender.com/api/v1',
 	resize_api_url: 'https://filmsdb-resizeimg-production.up.railway.app',
 };
