38bcd559fe808aeffa75bbabf077e63649f5db90
felipe gomez <ggfelipegc@gmail.com>
2023-08-14 09:20:20 -0500

    build: change host url server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 module.exports = {
   // films_api_url: 'http://localhost:8080/api/v1',
   // films_api_url: "https://filmsdb-ms-production.up.railway.app/api/v1",
-  //   films_api_url: "https://filsmdb-microservice.onrender.com/api/v1",
-  films_api_url: "https://filmsdbms-ggfelipegc.b4a.run/api/v1",
+  films_api_url: "https://filsmdb-microservice.onrender.com/api/v1",
+  // films_api_url: "https://filmsdbms-ggfelipegc.b4a.run/api/v1",
   resize_api_url: "https://filmsdb-resizeimg-production.up.railway.app",
 };
