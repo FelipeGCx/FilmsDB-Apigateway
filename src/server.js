f5c180882d6806160e7b708c5cc4adab1910e761
felipe gomez <ggfelipegc@gmail.com>
2023-09-23 20:35:15 -0500

    build: change image resize url

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ module.exports = {
   // films_api_url: "https://filmsdb-ms-production.up.railway.app/api/v1",
   films_api_url: "https://filsmdb-microservice.onrender.com/api/v1",
   // films_api_url: "https://filmsdbms-ggfelipegc.b4a.run/api/v1",
-  resize_api_url: "https://filmsdb-resizeimg-production.up.railway.app",
+  resize_api_url: "https://resize-service.vercel.app",
 };
