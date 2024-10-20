a32731eff79a543f3f17138c92585711a71ff211
felipegcx <ggfelipegc@gmail.com>
2024-10-20 17:23:31 -0500

    refactor: update backend api

--- src/server.js
+++ src/server.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // films_api_url: 'http://localhost:8080/api/v1',
-  // films_api_url: "https://filmsdb-ms-production.up.railway.app/api/v1",
-  films_api_url: "https://filsmdb-microservice.onrender.com/api/v1",
-  // films_api_url: "https://filmsdbms-ggfelipegc.b4a.run/api/v1",
+  films_api_url: "https://filmsdb-backend-production.up.railway.app/api/v1",
   resize_api_url: "https://resize-service.vercel.app",
 };
